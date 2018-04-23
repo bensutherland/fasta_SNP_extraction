dfd2b82e3fa372d1802f0647876e271b54b655a4
bensutherland <bensutherland7@gmail.com>
2018-04-23 08:49:47 -0700

    Add identify req region w BLAST Rscript

---
+++ 01_scripts/identify_req_region_w_BLAST.R
@@ -0,0 +1,158 @@
+# Identify a 200 bp window around a SNP for loci aligned to a reference genome
+# Specifically built for T. pacificus project by Ben Sutherland (DFO) 2018-03-26
+# As usual, use at own risk, no guarantees on usefulness
+
+# Todo #
+# order based on length or eval, then take the first as the one to keep..
+# Do this in BLAST, using '-max_target_seqs 1'
+
+#### Front Matter ####
+
+# Clean space
+#rm(list=ls())
+
+# Set working directory
+setwd("~/Documents/06_tpac/fasta_SNP_extraction")
+
+# Install packages
+#install.packages("stringr")
+library(stringr)
+
+
+
+#### 0. Input data ####
+# Import blast outfmt6 results
+data <- read.table("03_blast_out/input_loci_v_tpac_no_badchar_outfmt6_single_hit.txt")
+head(data)
+colnames(data) <- c("qname","ref.name","ident","align.leng","mismatch","gapopen","qstart","qend","sstart","send","eval","bitscore")
+head(data)
+
+# import contig length file
+contig.length <- read.table(file = "02_input_data/genome/ref_genome_seq_lengths.txt", sep = "\t", header = F)
+head(contig.length)
+colnames(contig.length) <- c("ref.name", "length")
+head(contig.length)
+
+
+#### 0.a Format Input data ####
+# Separate marker name from the position of the SNP
+split.mname <- str_split_fixed(string = data$qname, pattern = "-", n = 2)
+colnames(split.mname) <- c("mname","snp.pos")
+head(split.mname)
+
+# Combine new split name with original dataframe
+data.split <- cbind(data, split.mname)
+head(data.split)
+# Make snp.pos into a numeric value
+data.split[,"snp.pos"] <- as.numeric(as.character(data.split$snp.pos))
+str(data.split)
+
+# merge data
+dim(data.split)
+dim(contig.length)
+data.collected <- merge(x = data.split, y = contig.length, by = "ref.name")
+dim(data.collected)
+head(data.collected)
+
+# Rename the new split data as data
+data <- data.collected
+
+
+### Characterize input data
+length(unique(data$qname))
+length(data$qname)
+
+
+#### 1. Identify the location of the SNP on the reference genome ####
+## Important Note: 
+##  alignments may be forward or reverse aligned, but in either case, 
+##   the query will be in the forward direction, the subject may invert
+
+# Identify the location of the SNP on the ref genome
+snp.spot <- rep(NA, times = nrow(data))
+# approx.read.length <- 83 # estimated from overall distribution of read lengths
+
+for(i in 1:nrow(data)){
+  
+  # if forward
+  if(data$sstart[i] < data$send[i]){
+    snp.spot[i] <- (data$sstart[i] + (data$snp.pos[i] - data$qstart[i]))
+  } 
+  
+  # if reverse 
+  if(data$sstart[i] > data$send[i]){
+    snp.spot[i] <- (data$sstart[i] - (data$snp.pos[i] - data$qstart[i]))
+  }
+}
+
+head(snp.spot)
+
+# Attach to the rest of the data
+head(data)
+all.data <- cbind(data, snp.spot)
+
+head(all.data)
+
+
+#### 2. Find 200 bp window range ####
+
+# Identify the range, but when position is less than 100, take 0. 
+## MAY NOT BE TRUE note: there is nothing to save the windows that go over the length
+
+
+# rename as data2 (temporary)
+data2 <- all.data
+
+# How many targets are within 100 bp of start of contig?
+table(data2$snp.spot > 100) # note will count twice for ones with multiple hits per query
+
+begin.region <- rep(NA, times = nrow(data2))
+end.region <- rep(NA, times = nrow(data2))
+
+for(i in 1:nrow(data2)){
+  
+  # if snp.spot is greater than 100 bp into ref contig, do the full method
+  if(data2$snp.spot[i] > 100){
+    begin.region[i] <- data2$snp.spot[i] - 100 
+    end.region[i] <- data2$snp.spot[i] + 100 
+  } else {
+    
+    # if alignment position is not > 100 bp into the contig, then just start at the beginning of the contig
+    begin.region[i] <- 0
+    end.region[i] <- 200
+  }
+}
+
+head(begin.region)
+head(data2$snp.spot)
+head(end.region)
+
+# Collect into datafile
+data3 <- cbind(data2, begin.region, end.region)
+
+head(data3)
+
+# How many of data3 are beyond the sequence lenght of the contig?
+table(data3$end.region > data3$length)
+
+# Fix the issue of the end region being after the end of the contig:
+for(i in 1:nrow(data3)){
+  if(data3$end.region[i] > data3$length[i]){
+    data3$end.region[i] <- data3$length[i]
+    data3$begin.region[i] <- (data3$length[i] - 200)
+  }
+}
+
+
+# Plot where on the reference contig the data is
+hist(data3$begin.region, breaks = 200, main = "Start point of extracting region", xlab = "distance (bp)")
+
+# Export whole data file
+write.csv(data3, file = "04_extraction/ranges_for_amplicon_extraction.csv", row.names = F, quote = F)
+
+# Export as a bed file extractor
+data3.bed <- data3[,c("ref.name","begin.region","end.region")]
+head(data3.bed)
+write.table(x = data3.bed, file = "04_extraction/ranges_for_amplicon_extraction.bed", sep = "\t"
+            , col.names = F, row.names = F, quote = F)
+
