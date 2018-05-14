60a6a9305d46bb51e991101bf8a6c110e6b82277
bensutherland <bensutherland7@gmail.com>
2018-05-14 15:35:21 -0700

    Avoid hard-coded window size in both scripts

--- 01_scripts/rename_amplicons.R
+++ 01_scripts/rename_amplicons.R
@@ -15,8 +15,9 @@ library(stringr)
 # install.packages("tidyr")
 library(tidyr)
 
-
-
+## Important: set window size used in 'identify_req_region_w_BLAST.R'
+total.window <- as.numeric(read.table(file="04_extraction/total_window_size.txt"))
+total.window
 
 #### 0. Input data ####
 # Import blast outfmt6 results
@@ -25,10 +26,13 @@ head(data)
 colnames(data) <- c("match.id", "seq")
 head(data)
 
-# remove duplicates
+# Remove exact duplicates of aligned markers
+dim(data)
 data <- data[order(data[,'match.id']), ]
 data <- data[!duplicated(data$match.id), ]
-dim(data)
+dim(data) # removes several hits
+
+# Note that it doesn't matter which segment is deleted, as they are exact duplicates when removed
 
 # can separate the match.id if needed
 #data2 <- separate(data = data, col = "contig.name", into = c("contig", "range"), sep = "\\:")
@@ -41,12 +45,12 @@ match.id <- paste(data.suppl$ref.name,":",data.suppl$begin.region,"-",data.suppl
 data.suppl.all <- cbind(data.suppl, match.id)
 head(data.suppl.all)
 
-
-# remove duplicates
+# As above, remove exact duplicates of aligned markers
 data.suppl.all <- data.suppl.all[order(data.suppl.all[, 'match.id']), ]
 data.suppl.all <- data.suppl.all[!duplicated(data.suppl.all$match.id), ]
 dim(data.suppl.all)
 
+# Note that this matters more about which is deleted, because certain markers are more useful than others
 
 # Merge two dataframes
 data.all <- merge(x = data, y = data.suppl.all, by = "match.id")
@@ -55,35 +59,39 @@ dim(data.suppl.all)
 dim(data.all)
 head(data.all)
 
-# Determine exactly where the SNP is on the extracted window
+
+#### Find the snp.spot within the extracted window ####
 snp.pos.in.window <- data.all$snp.spot - data.all$begin.region 
 head(snp.pos.in.window)
 
+# connect to the larger df
 data.all2 <- cbind(data.all, snp.pos.in.window)
-
 colnames(data.all2)
 head(data.all2[,7:ncol(data.all2)], n = 20)
 
 
-###### Correct the snp.pos.in.window when reverse complementing
+#### Correct the snp.pos.in.window when reverse complementing ####
+# To match the marker file, when the reference genome has been reverse-complemented during the alignment (for.or.rev = rev),
+# Calculate where the snp will be in the window after it has been reverse complemented. 
+
 for(i in 1:nrow(data.all2)){
-  # Only correct if reverse
+  # If alignment is reversed, to prepare for reverse complementing amplicon window, subtract from total.window
   if(data.all2$for.or.rev[i]=="rev"){
-    data.all2$snp.pos.in.window[i] <- 200 - data.all2$snp.pos.in.window[i]
+    data.all2$snp.pos.in.window[i] <- total.window - data.all2$snp.pos.in.window[i]
   }
 }
 
 head(data.all2[,7:ncol(data.all2)], n = 20)
 
 
-###### Correct the specific issue of reverse complement SNP being at position 101 when title suggests 100
+###### Correct the specific issue of reverse complement SNP being at middle position
 colnames(data.all2)
-data.all2[ which(data.all2$for.or.rev=="rev" & data.all2$snp.pos.in.window=="100"), "snp.pos.in.window" ] <- 101
+data.all2[ which(data.all2$for.or.rev=="rev" & data.all2$snp.pos.in.window=="200"), "snp.pos.in.window" ] <- 201
 
 
 ###### Separate output for reverse complement target regions and forward regions
-dim(data.all2[data.all2$for.or.rev=="rev", ]) # 1935
-dim(data.all2[data.all2$for.or.rev=="for", ]) # 1971
+dim(data.all2[data.all2$for.or.rev=="rev", ]) # 1922
+dim(data.all2[data.all2$for.or.rev=="for", ]) # 1952
 
 
 # Get the needed pieces for the name
