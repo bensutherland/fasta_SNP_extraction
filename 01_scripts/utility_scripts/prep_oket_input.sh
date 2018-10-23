452d9f84e9f0a34e629a1cfca1623568d90a4c39
bensutherland <bensutherland7@gmail.com>
2018-10-23 14:26:58 -0700

    Add MGL SNPs to utility script to collect data

--- 01_scripts/utility_scripts/prep_oket_input.sh
+++ 01_scripts/utility_scripts/prep_oket_input.sh
@@ -10,9 +10,11 @@ SEEB_INPUT="Seeb"
 UW_INPUT="UW_GTseq"
 WDFW_INPUT="WDFW_GTseq"
 UW_SECOND_SET="UW_all" # this is to capture the 'not included' snps
+MGL_INPUT="MGL_chum_amplicons_w_alleles"
 
 # Date and extension
 DATE_EXT="2018-10-05.csv" # essentially the input extension
+DATE_EXT2="2018-10-23.csv" # to allow for a second date (MGL)
 OUTPUT_EXT="marker_and_seq.csv" # the output extension
 
 ## Put all inputs into the same format
@@ -51,8 +53,18 @@ grep -vE '^Panel,' $RAW_DATA/$UW_SECOND_SET"_"$DATE_EXT |
 # Replace the hyphen with an underscore to not disrupt future steps
 sed 's/\-/\_/g' $RAW_DATA/$WDFW_INPUT"_"$DATE_EXT > $RAW_DATA/$WDFW_INPUT"_"$OUTPUT_EXT
 
+# MGL input
+# Remove header
+grep -vE '^mname' $RAW_DATA/$MGL_INPUT"_"$DATE_EXT2 | 
+    
+    # Remove fasta symbol '>'
+    sed 's/>//g' - > $RAW_DATA/$MGL_INPUT"_"$OUTPUT_EXT
+
 ## Combine all data into one file
 cat $RAW_DATA/$SEEB_INPUT"_"$OUTPUT_EXT \
     $RAW_DATA/$UW_INPUT"_"$OUTPUT_EXT \
     $RAW_DATA/$WDFW_INPUT"_"$OUTPUT_EXT \
+    $RAW_DATA/$UW_SECOND_SET"_"$OUTPUT_EXT \
+    $RAW_DATA/$MGL_INPUT"_"$OUTPUT_EXT \
     > $RAW_DATA/"all_input_"$OUTPUT_EXT
+
