caa0e9eb3ee66542051aadeb20590d13f7939ef8
bensutherland <bensutherland7@gmail.com>
2019-09-26 14:12:56 -0700

    Fix method of switching markers when duplicates

--- 01_scripts/remove_pref_duplicates.R
+++ 01_scripts/remove_pref_duplicates.R
@@ -1,7 +1,6 @@
 ## Identify corresponding markers based on genomic coordinates
 # rm(list=ls())
 
-
 #### 01. Read in, join, and format ####
 # Read in all info (needed to pair markers from the deleted list to the retained list)
 data.suppl <- read.csv(file = "05_amplicons/dropped_duplicates_suppl_info.csv", stringsAsFactors = F)
@@ -36,6 +35,7 @@ for(i in 1:length(priority1.FNs)){
 }
 
 amp_mnames_priority1 <- priority1.df
+head(amp_mnames_priority1)
 dim(amp_mnames_priority1)
 table(amp_mnames_priority1$source) # these are the requested numbers of markers from each priority
 
@@ -51,6 +51,7 @@ for(i in 1:length(priority2.FNs)){
 }
 
 amp_mnames_priority2 <- priority2.df
+head(amp_mnames_priority2)
 dim(amp_mnames_priority2)
 table(amp_mnames_priority2$source) # these are the requested numbers of markers from each priority
 
@@ -58,8 +59,8 @@ table(amp_mnames_priority2$source) # these are the requested numbers of markers 
 #### Replace expected dropped markers that are from the priority lists with non-priority markers ####
 head(corresp_data)
 
-# fill vector with dummy character
-corresp_data$duplicated_name <- rep("no", times = nrow(corresp_data))
+# # fill vector with dummy character
+# corresp_data$duplicated_name <- rep("no", times = nrow(corresp_data))
 
 # Loop to correct the marker that will be dropped based on the priority list
 for(i in 1:nrow(corresp_data)){
@@ -113,6 +114,8 @@ for(i in 1:nrow(corresp_data)){
   
 }  
 
+head(corresp_data, n = 10)
+
 table(corresp_data$dropped.priority1)
 table(corresp_data$dropped.priority2)
 table(corresp_data$retained.priority1)
@@ -129,13 +132,13 @@ for(i in 1:nrow(corresp_data)){
   # If the marker dropped is priority one, and the retained is not, switch
   if(corresp_data$dropped.priority1[i]=="yes" & corresp_data$retained.priority1[i]=="no"){
     
-    # Switch the dropped to the retained
+    # Switch the retained marker into the drop column
     corresp_data$marker_to_drop[i] <- corresp_data$mname.retained[i]
     
     # Reporting
     corresp_data$switched[i] <- "true-keep_pri1"
   
-  # If the marker dropped is priorty one, and the retained marker is priority one, do nothing  
+  # If the marker dropped is priority one, and the retained marker is also priority one, do nothing  
   } else if(corresp_data$dropped.priority1[i]=="yes" & corresp_data$retained.priority1[i]=="yes"){
     
     # do nothing
@@ -144,9 +147,9 @@ for(i in 1:nrow(corresp_data)){
     corresp_data$switched[i] <- "cant_both_are_pri_1"
   
   # If the marker dropped is not priority one, and the marker dropped is priority two, then switch
-  } else if(corresp_data$dropped.priority1[i]=="no" & corresp_data$dropped.priority2[i]=="yes"){
+  } else if(corresp_data$dropped.priority1[i]=="no" & corresp_data$dropped.priority2[i]=="yes" & corresp_data$retained.priority1[i]=="no"){
     
-    # switch the dropped to the retained
+    # switch the retained marker to the drop column
     corresp_data$marker_to_drop[i] <- corresp_data$mname.retained[i]
     
     # Reporting
@@ -160,17 +163,26 @@ for(i in 1:nrow(corresp_data)){
     # Reporting
     corresp_data$switched[i] <- "false"
     
+  # 
+  } else if(corresp_data$dropped.priority1[i]=="no" & corresp_data$retained.priority1[i]=="yes"){
+    
+    # Keep with the planned drop marker
+    corresp_data$marker_to_drop[i] <- corresp_data$mname.dropped[i]
+    
+    # Reporting
+    corresp_data$switched[i] <- "false"
+    
   }
-  
 }
 
 
-head(corresp_data, n = 10)
+head(corresp_data[,-1], n = 10)
 table(corresp_data$switched)
 
 
 # If can't drop either as they are both priority 1 (could be because same name, or both markers are in priority one, then 
-#  remove it from the drop list and let the duplicate screen remove the duplicate
+#  remove it from the drop list and let the duplicate screen remove the duplicate at random 
+#  This should not happen if one of the markers is non-priority 1
 dim(corresp_data)
 corresp_data <- corresp_data[corresp_data$switched!="cant_both_are_pri_1", ]
 dim(corresp_data)
